70208c1fde1b799858079d7e2ec5e1782edefc85
CYOSP <cyosp@cyosp.com>
2020-05-18 08:15:08 +0200

    Remove macOS useless code

--- desktop/app.js
+++ desktop/app.js
@@ -6,13 +6,6 @@ let browserWindow;
 app.whenReady().then(() => {
     trayIcon.build();
     startGoogleChat();
-
-    app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0)
-            startGoogleChat();
-    });
 });
 
 function startGoogleChat() {
@@ -32,10 +25,6 @@ function loadGoogleChat() {
     browserWindow.loadURL("https://chat.google.com");
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin')
-        app.quit();
+    app.quit();
 });
