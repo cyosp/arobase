39ef40a834c6ffe12104cfaedc7c7aa482380e9d
CYOSP <cyosp@cyosp.com>
2020-05-20 07:23:44 +0200

    Compute status based on favicon hyperlink

--- desktop/event-handler.js
+++ desktop/event-handler.js
@@ -1,5 +1,17 @@
 const {ipcMain} = require("electron");
 
-ipcMain.on("favicon-changed", (evt, href) => {
-    console.debug("Favicon href: " + href)
+ipcMain.on("favicon-changed", (ipcMainEvent, href) => {
+    let status = computeStatus(href);
+    console.debug("Status: " + status);
 });
+
+function computeStatus(href) {
+    let status = "offline";
+    if (href.match(/new-notif/))
+        status = "personal-notification";
+    else if (href.match(/new-non-notif/))
+        status = "channel-notification";
+    else if (href.match(/no-new/))
+        status = "online";
+    return status;
+}
